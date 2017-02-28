f587d50b8087388ea86491a292620bb9044dfbab
Y2PM <joesamban@yahoo.co.uk>
2017-02-28 11:20:56 +0000

    Started using Win32_NetworkAdapterConfiguration

---
+++ ClassLibrary1/IpConfigChangerClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class IpConfigChangerClass
+    {
+        public void changeSettings(string NewIpAddress)
+        {
+            ManagementClass ManagementClassObject = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection ManagementObjectCollectionObject = ManagementClassObject.GetInstances();
+
+            foreach (ManagementObject item in ManagementObjectCollectionObject)
+            {
+                
+            }
+
+        }
+    }
+}
---
+++ ClassLibrary1_Test/IpConfigChangerClass_Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassLibrary1_Test
+{
+    [TestClass]
+    public class IpConfigChangerClass_Test
+    {
+        [TestMethod]
+        public void Test_changeSettingsMethod_ChangesIPv4Adress_WhenGivenANewIPv4Address()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
